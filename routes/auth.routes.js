c636f7c5846a1a34ca66fa1559562da57839284a
bresler <bmoralesg25@miumg.edu.gt>
2025-09-26 19:33:47 -0600

    Revert "MI PARTE BRESLER"
    This reverts commit 07feed8eb7f150bda52d45df0ed1c0b05a881152.

--- routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-  const auth = require('../controllers/auth.controller.js');
-  const router = require('express').Router();
-
-  router.post('/login', auth.login);
-
-  app.use('/api/auth', router);
-};
--- server.js
+++ server.js
@@ -1,28 +1,11 @@
 const express = require('express');
-require('dotenv').config();
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 const db = require('./app/models');
 
+// Middlewares y rutas aquí...
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
-
-app.get('/', (req, res) => {
-  res.json({ message: "Bienvenido a la API del cine." });
-});
-
-require('./app/routes/auth.routes')(app);
-require('./app/routes/usuarios.routes')(app);
-require('./app/routes/promos.routes')(app);
-require('./app/routes/user.routes')(app);
-
-
-
-db.sequelize.sync({ force: false })
+db.sequelize.sync({ force: true })
   .then(() => {
     console.log('Base de datos sincronizada');
     app.listen(PORT, () => {
@@ -32,3 +15,4 @@ db.sequelize.sync({ force: false })
   .catch(err => {
     console.error('Error al sincronizar la base de datos:', err);
   });
+  
