180e2016bbb13a576358348ea0c4aa0c33ddc5bc
Eduardo Cam <eduardocamaja213@gmail.com>
2025-10-24 00:25:02 -0600

    Limpiar raíz: dejar solo backend/ y frontend/ en la raíz

--- app/models/asientos.model.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Asiento = sequelize.define('Asiento', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    salaId: { type: DataTypes.INTEGER, allowNull: false },
-    fila: { type: DataTypes.STRING, allowNull: false },
-    numero: { type: DataTypes.INTEGER, allowNull: false }
-  }, { tableName: 'asientos', timestamps: false });
-  return Asiento;
-};
--- app/models/estrenos.model.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Estreno = sequelize.define('Estreno', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    peliculaId: { type: DataTypes.INTEGER, allowNull: false },
-    fecha_estreno: { type: DataTypes.DATE, allowNull: false }
-  }, { tableName: 'estrenos', timestamps: true });
-  return Estreno;
-};
